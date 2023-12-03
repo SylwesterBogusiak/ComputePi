7d8fd7754bb5b1321dabd9785aafa804efba65ee
MARTE.BEST <122264765+SylwesterBogusiak@users.noreply.github.com>
2023-12-03 19:24:30 +0100

    Update pill.c

--- pill.c
+++ pill.c
@@ -21,7 +21,8 @@
 #include <time.h>
 #endif
 
-int pill(char *stop, unsigned int *bt){
+int pill(char *stop, unsigned int *bt)
+{
 
     /* Applying Classic Leibniz Formula */
     
@@ -36,22 +37,19 @@ int pill(char *stop, unsigned int *bt){
  printf("\nPI = %.16lf \n", pi);
 */
  
-   mpfr_t sum3, term3, pi3, i3, n3, x3, i2, one3,two3, power3, div3, div33;
-    
-   mpfr_inits2 (*bt, sum3, term3, pi3, i3, n3, x3, i2, one3, two3, power3, div3, div33, NULL);
+   	mpfr_t sum3, term3, pi3, i3, n3, x3, i2, one3,two3, power3, div3, div33;
+	mpfr_inits2 (*bt, sum3, term3, pi3, i3, n3, x3, i2, one3, two3, power3, div3, div33, NULL);
 
- 
-  
-   	mpfr_set_si (i3, 0, MPFR_RNDD);
- 	  mpfr_set_si(x3, 0, MPFR_RNDD);
- 	  mpfr_set_d (sum3, 0.0, MPFR_RNDD);
- 	  mpfr_set_d (term3, 0.0, MPFR_RNDD);
-  	mpfr_set_d (div3, 0.0, MPFR_RNDD);
-    mpfr_set_si (one3, -1, MPFR_RNDD);
- 	  mpfr_set_si (two3, 2, MPFR_RNDD);
-    mpfr_set_d (pi3, 0.0, MPFR_RNDD);
-    mpfr_set_str (n3, stop, 10, MPFR_RNDD);
-	  mpfr_sub_si(i2, n3, 1, MPFR_RNDD);
+	mpfr_set_si (i3, 0, MPFR_RNDD);
+	mpfr_set_si(x3, 0, MPFR_RNDD);
+	mpfr_set_d (sum3, 0.0, MPFR_RNDD);
+	mpfr_set_d (term3, 0.0, MPFR_RNDD);
+	mpfr_set_d (div3, 0.0, MPFR_RNDD);
+	mpfr_set_si (one3, -1, MPFR_RNDD);
+	mpfr_set_si (two3, 2, MPFR_RNDD);
+	mpfr_set_d (pi3, 0.0, MPFR_RNDD);
+	mpfr_set_str (n3, stop, 10, MPFR_RNDD);
+	mpfr_sub_si(i2, n3, 1, MPFR_RNDD);
 	
 	
  /* Applying Extended Long Leibniz Formula */ // PILL - PI LONG LEIBNIZ
@@ -59,24 +57,25 @@ int pill(char *stop, unsigned int *bt){
  while(mpfr_cmpabs(i3,n3)<0)
  
  {     
-	  mpfr_sub_si(i3, i3, 1, MPFR_RNDD);
+	mpfr_sub_si(i3, i3, 1, MPFR_RNDD);
 
-	  mpfr_pow (power3, one3, x3, MPFR_RNDD);  
-	  mpfr_mul(div3, two3, x3, MPFR_RNDD);  
-	  mpfr_add_si(div3,div3,1,MPFR_RNDD);      
-	  mpfr_div(term3,power3,div3,MPFR_RNDD);           
+	mpfr_pow (power3, one3, x3, MPFR_RNDD);  
+	mpfr_mul(div3, two3, x3, MPFR_RNDD);  
+	mpfr_add_si(div3,div3,1,MPFR_RNDD);      
+	mpfr_div(term3,power3,div3,MPFR_RNDD);           
 
    	mpfr_add(sum3,sum3,term3,MPFR_RNDD);        
-   	mpfr_add_si(x3, x3, 1, MPFR_RNDD);
+	mpfr_add_si(x3, x3, 1, MPFR_RNDD);
 
-         //Print out last - 1 result // for odd numbers
+         
         if ((mpfr_cmpabs(i3,i2)>0))
         {
         mpfr_mul_si(pi3,sum3,4,MPFR_RNDD);
-        mpfr_printf ("\n===================\nPI for (n = %.*RZf): ",0,i2);
-     
-    	  mpfr_out_str (stdout, 10, *bt, pi3, MPFR_RNDD);
-    	  printf ("\n===================\n\n");
+		
+        //Print out last - 1 result // for odd numbers
+	mpfr_printf ("\n===================\nPI for (n = %.*RZf): ",0,i2);
+	mpfr_out_str (stdout, 10, *bt, pi3, MPFR_RNDD);
+    	printf ("\n===================\n\n");
         }
  
  
@@ -84,12 +83,12 @@ int pill(char *stop, unsigned int *bt){
  }
   	mpfr_mul_si(pi3,sum3,4,MPFR_RNDD);
   
-    //Print out last result // for even numbers
-    mpfr_printf ("\n===================\nPI for (n = %.*RZf): ",0,i3);
-    mpfr_out_str (stdout, 10, *bt, pi3, MPFR_RNDD);
-    printf ("\n===================\n\n");
+    	//Print out last result // for even numbers
+    	mpfr_printf ("\n===================\nPI for (n = %.*RZf): ",0,i3);
+    	mpfr_out_str (stdout, 10, *bt, pi3, MPFR_RNDD);
+    	printf ("\n===================\n\n");
     
-    mpfr_clears (sum3, term3, pi3, i3, n3, x3, i2, one3,two3, power3, div3, div33, NULL);
+    	mpfr_clears (sum3, term3, pi3, i3, n3, x3, i2, one3,two3, power3, div3, div33, NULL);
 
     return 0;
 }
